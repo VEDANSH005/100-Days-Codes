0aa3c975fc77c81b21025aec5706075057bb3f88
VEDANSH005 <rawatvedans@gmail.com>
2025-09-30 16:11:47 +0530

    Write a program to check if a number is a perfect number.

---
+++ day21_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+int num, i,x, sum=0; 
+printf ("Enter a number:"); 
+scanf ("%d",&num);
+x=num;
+for(i=1;i<x;i++){
+    if (x%i==0){
+        sum=sum+i;
+    }
+}
+if (sum==num){
+    printf("Perfect Number \n");
+}
+else{
+    printf("Not a perfect number \n");
+}
+return 0;
+}
