d4474aeabcf0e9b772dfaa44d4b7824b2f8361c1
VEDANSH005 <rawatvedans@gmail.com>
2025-12-02 22:52:43 +0530

    Use malloc() to allocate structure memory dynamically and print details.

---
+++ day99.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+int main() 
+{
+    struct Student *s;
+    s = (struct Student*) malloc(sizeof(struct Student));
+    if (s == NULL) 
+    {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+    printf("Enter Name, Roll, Marks:\n");
+    scanf("%s %d %d", s->name, &s->roll, &s->marks);
+    printf("Name: %s | Roll: %d | Marks: %d\n", s->name, s->roll, s->marks);
+    free(s);
+    return 0;
+}
