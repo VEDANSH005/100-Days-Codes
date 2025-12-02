b442e38c0e11c534d1a6a1303ff811e08f56edad
VEDANSH005 <rawatvedans@gmail.com>
2025-12-02 21:18:23 +0530

    Count vowels and consonants in a string.

---
+++ day42_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>// for tolower ()
+int main () {
+char str [100];
+int vowels = 0, consonants = 0, i = 0;
+printf("Enter a string: ");
+fgets (str, sizeof(str), stdin);
+while (str[i] != "10"){
+char ch = tolower(str[i]);// convert to lowercase for easy checking
+if (ch >= 'a' && ch <= 'z') {// check only alphabets
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        vowels++;
+    else 
+        consonants++;
+}
+i++;
+}
+printf( "Vowels=%d, Consonants=%d\n", vowels, consonants);
+return 0;
+}
