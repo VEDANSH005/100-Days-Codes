bed37bdef9a6c937338be7d5034c1d180c812bc7
VEDANSH005 <rawatvedans@gmail.com>
2025-12-02 23:00:16 +0530

    Delete day3_1.c

--- day3_1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int celcius,farahenit;
-    printf("enter temperature in celcius=");
-    scanf("%d",&celcius);
-    farahenit=(celcius*9/5)+32;
-    printf("temperature in farahenit=%d\n",farahenit);
-    return 0;
-}
