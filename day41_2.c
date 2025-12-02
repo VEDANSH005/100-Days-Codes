9d000764ebac3c332f2f4e1a75ef74a36945f6f6
VEDANSH005 <rawatvedans@gmail.com>
2025-12-02 21:17:43 +0530

    Print each character of a string on a new line.

---
+++ day41_2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+char str [100];
+int count = 0;
+printf("Enter a string: ");
+fgets(str, sizeof(str), stdin);//read input including spaces
+//count characters one by one until '\0' 
+while (str[count] != "\0"){
+    {
+        printf ("%c",str[count]) ;
+    }
+    printf ("\n");
+    count ++;
+}
+return 0;
+}
