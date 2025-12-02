f80d600ebc21404737856dc267b86f58fd5e4bfa
VEDANSH005 <rawatvedans@gmail.com>
2025-12-02 22:41:13 +0530

    Store details of 5 students in an array of structures and print all.

---
+++ day92.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    struct Student s[5];
+
+    // Reading details of 5 students
+    for (int i = 0; i < 5; i++) {
+        scanf("%s %d %d", s[i].name, &s[i].roll, &s[i].marks);
+    }
+
+    // Printing in tabular format
+    printf("Name\tRoll\tMarks\n");
+
+    for (int i = 0; i < 5; i++) {
+        printf("%s\t%d\t%d\n", s[i].name, s[i].roll, s[i].marks);
+    }
+
+    return 0;
+}
